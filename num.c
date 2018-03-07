ee15c3a7871f0117694f57f50fe51568c5abdb94
selvarajrojaajith <36071813+selvarajrojaajith@users.noreply.github.com>
2018-03-07 16:23:48 +0530

    num.c
    good

---
+++ num.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+int 1,2;
+printf("\n enter the first number");
+scanf("%d",first number);
+printf("\n enter the second number");
+scanf("%d",second number);
+printf("Enter the swaping two numbers")
+return 0
+}
